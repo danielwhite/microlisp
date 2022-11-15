0f383eae922b1e8d3d5501c161e89469373b3908
Daniel White <daniel@whitehouse.id.au>
2022-11-16 00:56:51 +1100

    Reformat package documentation with Go 1.19

--- doc.go
+++ doc.go
@@ -3,7 +3,7 @@ Microlisp is a Lisp interpreter. It's primarily a toy, and is a work in progress
 
 Its initial definition is based on McCarthy's "A Micro-Manual for Lisp: Not the Whole Truth".
 
-Environments
+# Environments
 
 An environment is a value that maintains mappings between a symbol and
 a value. If an environment has a parent, then bindings are inherited.
@@ -12,21 +12,21 @@ The evaluator typically provides an environment implicitly.
 
 Variables.
 
-  system-environment	Primitives are bound in this environment.
-  user-environment	User definitions are bound here, and it inherits system-environment.
+	system-environment	Primitives are bound in this environment.
+	user-environment	User definitions are bound here, and it inherits system-environment.
 
 Functions.
 
-  environment-bindings	The environment's bindings represented as an association list.
+	environment-bindings	The environment's bindings represented as an association list.
 
-Errors
+# Errors
 
 The error system is very simple. If an error value is thrown, it stops
 the current evaluation, and is handled and printed by the REPL.
 
-Functions.
+# Functions
 
-  error		Raise an error value with a message composed of its arguments.
-  ignore-errors	Invoke a function, trapping any errors thrown as a return value.
+	error		Raise an error value with a message composed of its arguments.
+	ignore-errors	Invoke a function, trapping any errors thrown as a return value.
 */
 package main // import "whitehouse.id.au/microlisp"
